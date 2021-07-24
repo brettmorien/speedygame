60b942b82f5923a74c2eb8425f6ab3ccf2e110c0
Brett Morien <brett@facetdev.com>
2021-07-24 16:23:25 -0700

    Factor out game and player class add more road lines fps borders road lines

--- engine.js
+++ engine.js
@@ -1,63 +1,91 @@
-var canvas;
+var canvas
 
 function startGame() {
-  console.log("starting game loop");
+  console.log('starting game loop')
+  game = new Game()
 
-  window.requestAnimationFrame(drawScene);
+  window.requestAnimationFrame(drawScene)
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  canvas = document.getElementById("game");
-});
+document.addEventListener('DOMContentLoaded', () => {
+  canvas = document.getElementById('game')
+})
+
+var previousTime = 1
+var game
+
+class Game {
+  distance = 0
+  player = new Player()
+}
+
+class Player {
+  speed = 47
+}
 
 function drawScene(time) {
-  debugLine = 0;
-  let ctx = canvas.getContext("2d")
+  let ctx = canvas.getContext('2d')
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  horizon = 60
-  stripeLength = 20
-  speed = 47
-  distance = (time / 1000) * speed
+  let hz = (1000 / (time - previousTime)).toFixed(2)
+  previousTime = time
+  debug(`${hz} fps`)
 
-  // debug(time)
-  // debug(distance)
+  horizon = 80
+  stripeLength = 20
+  game.distance = (canvas.height + (time / 1000) * game.player.speed).toFixed(2)
+
+  debug(`${(time / 1000).toFixed(2)}s`)
+  debug(`d: ${game.distance}`)
   center = canvas.width / 2
 
   for (let i = canvas.height; i >= horizon; i--) {
-    ctx.fillStyle =
-      Math.abs(distance - i) % stripeLength >= stripeLength / 2 ? "grey" : "black";
-    // ctx.fillStyle = (i + distance) % 10 >= 5 ? "grey" : "black"
+    let odd = (game.distance - i) % stripeLength >= stripeLength / 2
+
+    ctx.fillStyle = odd ? 'grey' : 'darkgrey'
     ctx.fillRect(center - i / 2, i, i, 1)
+
+    ctx.fillStyle = odd ? 'red' : 'white'
+    ctx.fillRect(center - i / 2 - 10, i, 10, 1)
+    ctx.fillRect(center + i / 2, i, 10, 1)
+
+    if (odd) {
+      ctx.fillStyle = "#eeeeee"
+      ctx.fillRect(center - i / 4 - 10, i, 10, 1)
+      ctx.fillRect(center + i / 4, i, 10, 1)
+    }
   }
 
-  // ctx.fillRect(10, 10, 50, 50)
-  // ctx.fillStyle = "rgba(0, 0, 200, 0.5)"
-  // ctx.fillRect(30, 30, 50, 50)
-
-  // ctx.beginPath()
-  // ctx.strokeStyle = "blue"
-  // ctx.moveTo(20, 20)
-  // ctx.lineTo(200, 20)
-  // ctx.stroke()
-
-  // // Second path
-  // ctx.beginPath()
-  // ctx.strokeStyle = "green"
-  // ctx.moveTo(20, 20)
-  // ctx.lineTo(120, 120)
-  // ctx.stroke()
-
-  ctx.strokeStyle = "black";
-  // ctx.strokeText(time, 20, 20, 200)
-  // ctx.strokeText(`${canvas.width} x ${canvas.height}`, 20, 60, 200)
-
-  window.requestAnimationFrame(drawScene);
+  resetDebug()
+  window.requestAnimationFrame(drawScene)
 }
 
-var debugLine = 0;
+var debugLine = 0
+function resetDebug() {
+  debugLine = 0
+}
 function debug(text) {
-  let ctx = canvas.getContext("2d");
-  ctx.strokeStyle = "black";
-  ctx.strokeText(text, 10, 10 * ++debugLine, 200);
+  let ctx = canvas.getContext('2d')
+  ctx.strokeStyle = 'black'
+  ctx.strokeText(text, 10, 10 * ++debugLine, 200)
 }
+
+// ctx.fillRect(10, 10, 50, 50)
+// ctx.fillStyle = "rgba(0, 0, 200, 0.5)"
+// ctx.fillRect(30, 30, 50, 50)
+
+// ctx.beginPath()
+// ctx.strokeStyle = "blue"
+// ctx.moveTo(20, 20)
+// ctx.lineTo(200, 20)
+// ctx.stroke()
+
+// // Second path
+// ctx.beginPath()
+// ctx.strokeStyle = "green"
+// ctx.moveTo(20, 20)
+// ctx.lineTo(120, 120)
+// ctx.stroke()
+
+// ctx.strokeText(time, 20, 20, 200)
+// ctx.strokeText(`${canvas.width} x ${canvas.height}`, 20, 60, 200)
