9cd77763b0f878cb7547654ad50c9edcaaa77e5e
Brett Morien <brett@facetdev.com>
2021-07-27 23:02:32 -0700

    Tessarossa sprite map up res

--- engine.js
+++ engine.js
@@ -12,7 +12,8 @@ function startGame() {
 document.addEventListener('DOMContentLoaded', () => {
   canvas = document.getElementById('game')
   game.horizon = canvas.height * 0.5
-  buildRoadZMap()
+  game.buildRoadZMap()
+  game.buildPlayerSpritesheet()
 })
 
 var previousTime = 0
@@ -24,13 +25,53 @@ class Game {
 
   cameraHeight = 50
   roadWidth = 200
+  playerScale = 5
 
   player = new Player()
+
+  roadZmap = []
+  playerSprites = []
+
+  buildRoadZMap() {
+    for (let i = 0; i < canvas.height; i++) {
+      this.roadZmap[i] = -game.cameraHeight / (i - game.horizon)
+    }
+  }
+
+  scale(size, y) {
+    return size / this.roadZmap[y]
+  }
+
+  buildPlayerSpritesheet() {
+    let img = document.getElementById('terrarossa')
+
+    this.playerSprites = [
+      new PlayerSprite(img,   8, 10, 40, 37), // R
+      new PlayerSprite(img,  56, 10, 40, 37),
+      new PlayerSprite(img, 104, 10, 40, 37),
+      new PlayerSprite(img, 152, 10, 40, 37), // M
+      new PlayerSprite(img, 200, 10, 40, 37),
+      new PlayerSprite(img, 248, 10, 40, 37),
+      new PlayerSprite(img, 296, 10, 40, 37), // L
+      new PlayerSprite(img, 344, 10, 40, 37),
+      new PlayerSprite(img, 392, 10, 40, 37),
+    ]
+  }
 }
 
 class Player {
   speed = 47
   input = new Input()
+}
+
+class PlayerSprite {
+  constructor(img, x, y, width, height) {
+    this.img = img
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+  }
 }
 
 class Input {
@@ -44,16 +85,16 @@ function keyDown(event) {
   switch (event.keyCode) {
     case 37:
       game.player.input.left = true
-      break;
+      break
     case 38:
       game.player.input.up = true
-      break;
+      break
     case 39:
       game.player.input.right = true
-      break;
+      break
     case 40:
       game.player.input.down = true
-      break;
+      break
   }
 }
 
@@ -61,23 +102,23 @@ function keyUp(event) {
   switch (event.keyCode) {
     case 37:
       game.player.input.left = false
-      break;
+      break
     case 38:
       game.player.input.up = false
-      break;
+      break
     case 39:
       game.player.input.right = false
-      break;
+      break
     case 40:
       game.player.input.down = false
-      break;
+      break
   }
 }
 
 function gameLoop(time) {
   resetDebug()
 
-  game.distance = (canvas.height + (time / 1000) * game.player.speed).toFixed(2)
+  game.distance = canvas.height + (time / 1000) * game.player.speed
 
   handleInput(time)
   drawScene(time)
@@ -89,14 +130,6 @@ function handleInput(time) {
     game.player.speed = 100
   } else {
     game.player.speed = 50
-  }
-}
-
-let roadZmap = []
-
-function buildRoadZMap() {
-  for (let i = 0; i < canvas.height; i++) {
-    roadZmap[i] = -game.cameraHeight / (i - game.horizon)
   }
 }
 
@@ -117,21 +150,22 @@ function drawScene(time) {
 
   var odd = false
   let stripeHeight = 8
-  var nextStripe = stripeHeight - scale(game.distance % (stripeHeight * 2), 0)
+  var nextStripe = stripeHeight - game.scale(game.distance % (stripeHeight * 2), 0)
+
   for (let i = 0; i < game.horizon; i++) {
     let y = canvas.height - i
-    let width = scale(game.roadWidth, i)
+    let width = game.scale(game.roadWidth, i)
 
     if (i > nextStripe) {
-      nextStripe = nextStripe + scale(stripeHeight, i)
+      nextStripe = nextStripe + game.scale(stripeHeight, i)
       odd = !odd
     }
 
     ctx.fillStyle = odd ? 'grey' : 'darkgrey'
     ctx.fillRect(center - width / 2, y, width, 1)
 
-    let borderWidth = scale(20, i)
-    let stripeWidth = scale(5, i)
+    let borderWidth = game.scale(20, i)
+    let stripeWidth = game.scale(5, i)
 
     ctx.fillStyle = odd ? 'white' : 'red'
     ctx.fillRect(center - width / 2 - borderWidth, y, borderWidth, 1)
@@ -143,10 +177,20 @@ function drawScene(time) {
       ctx.fillRect(center + width / 5, y, stripeWidth, 1)
     }
   }
-}
 
-function scale(size, y) {
-  return size / roadZmap[y]
+  let mult = 60
+
+  let spriteIndex = (game.distance / mult).toFixed(0) % game.playerSprites.length
+
+  debug(spriteIndex)
+
+  with (game.playerSprites[spriteIndex]) {
+    ctx.drawImage(img, x, y, width, height,
+      (canvas.width - width * game.playerScale) / 2,
+      canvas.height - height * game.playerScale - 20,
+      width * game.playerScale, height * game.playerScale
+    )
+  }
 }
 
 var debugLine = 0
